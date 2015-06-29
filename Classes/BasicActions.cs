a9a6c5cb229b74ac5a4525a8e125774ee9bc9097
Rune Lippert <runelippert@gmail.com>
2015-06-29 21:43:44 +0200

    Refacotred basic actions

---
+++ Classes/BasicActions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class BasicActions
+    {
+        public moves move { get; set; }
+        public enum moves
+        {
+            forward,
+            left,
+            rigth,
+            back
+        }
+
+        public static Coordinate movePlayer(Player playerToMove, moves order)
+        {
+            Coordinate currentPosition = playerToMove.position;
+
+            BasicActions.moves moveTo = order;
+
+            if (moveTo == BasicActions.moves.forward)
+            {
+                Console.WriteLine("{0} trying to go forward", playerToMove.name);
+                playerToMove.position.y++;
+                return playerToMove.position;
+            }
+            if (moveTo == BasicActions.moves.left)
+            {
+                Console.WriteLine("{0} trying to go left", playerToMove.name);
+                playerToMove.position.x--;
+                return playerToMove.position;
+            }
+            if (moveTo == BasicActions.moves.rigth)
+            {
+                Console.WriteLine("{0} trying to go rigth", playerToMove.name);
+                playerToMove.position.x++;
+                return playerToMove.position;
+            }
+            if (moveTo == BasicActions.moves.back)
+            {
+                Console.WriteLine("{0} trying to go back", playerToMove.name);
+                playerToMove.position.y--;
+                return playerToMove.position;
+            }
+            else
+            {
+                Console.WriteLine("Could not understand player move");
+                return playerToMove.position;
+            }
+        }
+   
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -40,60 +40,6 @@ namespace Classes
         
     }
 
-    /// <summary>
-    /// Basic actions that if allowed, any player can use at any time. 
-    /// </summary>
-    class BasicActions
-    {
-        public moves move { get; set; }
-        public enum moves
-        {
-            forward,
-            left,
-            rigth,
-            back
-        }
-
-        public static Coordinate movePlayer(Player playerToMove, moves order)
-        {
-            Coordinate currentPosition = playerToMove.position;
-
-            BasicActions.moves moveTo = order;
-
-            if (moveTo == BasicActions.moves.forward)
-            {
-                Console.WriteLine("{0} trying to go forward", playerToMove.name);
-                playerToMove.position.y++;
-                return playerToMove.position;
-            }
-            if (moveTo == BasicActions.moves.left)
-            {
-                Console.WriteLine("{0} trying to go left", playerToMove.name);
-                playerToMove.position.x--;
-                return playerToMove.position;
-            }
-            if (moveTo == BasicActions.moves.rigth)
-            {
-                Console.WriteLine("{0} trying to go rigth", playerToMove.name);
-                playerToMove.position.x++;
-                return playerToMove.position;
-            }
-            if (moveTo == BasicActions.moves.back)
-            {
-                Console.WriteLine("{0} trying to go back", playerToMove.name);
-                playerToMove.position.y--;
-                return playerToMove.position;
-            }
-            else
-            {
-                Console.WriteLine("Could not understand player move");
-                return playerToMove.position;
-            }         
-        }
-    }
-
-    
-
     
     class Program
     {
